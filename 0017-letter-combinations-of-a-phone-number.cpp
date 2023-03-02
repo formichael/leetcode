ac6d7ed6f1492d32c21ece38c2ea8fff40c03f9c
Michael <1906030+formichael@users.noreply.github.com>
2023-03-02 22:29:26 +0800

    Update 0017-letter-combinations-of-a-phone-number.cpp

--- 0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number.cpp
@@ -37,3 +37,47 @@ public:
         return out;
     }
 };
+
+class Solution {
+public:
+    map<char, vector<string>> dict = {
+        {'2', {"a", "b", "c"}},
+        {'3', {"d", "e", "f"}},
+        {'4', {"g", "h", "i"}},
+        {'5', {"j", "k", "l"}},
+        {'6', {"m", "n", "o"}},
+        {'7', {"p", "q", "r", "s"}},
+        {'8', {"t", "u", "v"}},
+        {'9', {"w", "x", "y", "z"}},
+    };
+
+    void doCombinations(string item, string digits, vector<string> &out)
+    {
+        if (digits.empty()) {
+            if (!item.empty()) {
+                out.push_back(item);
+            }
+            return;
+        }
+
+        auto a = dict[digits[0]];
+        
+        // digits.size() > 1
+        for (auto s : a) {
+            item.append(s);
+            doCombinations(item, digits.substr(1), out);
+            item.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> out;
+        if (digits.size() == 0) {
+            return out;
+        }
+
+        string item;
+        doCombinations(item, digits, out);
+        return out;
+    }
+};
