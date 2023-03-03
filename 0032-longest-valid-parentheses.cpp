df1e6fbc6ba489ca4518cfeeaf90e2ad81bc2cf2
Michael <1906030+formichael@users.noreply.github.com>
2023-03-03 23:53:03 +0800

    Update 0032-longest-valid-parentheses.cpp

--- 0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses.cpp
@@ -69,3 +69,31 @@ public:
        return cnt;
     }
 };
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int len = s.size();
+        if (len < 2) {
+            return 0;
+        }
+
+        int cnt = 0;
+        vector<int> dp(len, 0);
+
+        for (int i = 1; i < len; ++i) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                    cnt = max(cnt, dp[i]);
+                }
+                else if (i - 1 - dp[i - 1] >= 0 && s[i - 1 - dp[i - 1]] == '(') { // eg ...))
+                    dp[i] = dp[i - 1] + 2 + (i - 2 - dp[i - 1] >= 0 ? dp[i - 2 - dp[i - 1]] : 0);
+                    cnt = max(cnt, dp[i]);
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
