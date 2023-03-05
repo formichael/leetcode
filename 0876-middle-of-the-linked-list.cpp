ad4f365cca442c69df666f09ef904047b4262a8c
Michael <1906030+formichael@users.noreply.github.com>
2023-03-05 16:22:23 +0800

    Update 0876-middle-of-the-linked-list.cpp

--- 0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list.cpp
@@ -4,6 +4,23 @@ public:
         if (head == nullptr)
             return nullptr;
 
+        ListNode *slow = head, *fast = head;
+        while (fast) {
+            if (!fast->next) break; // arrived at the end of the list
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == nullptr)
+            return nullptr;
+
         vector<ListNode*> v;
         ListNode *cur = head;
         while (cur) {
