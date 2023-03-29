00470811fddfe52bbf8134f7ef276e4e13311792
Michael <1906030+formichael@users.noreply.github.com>
2023-03-29 17:58:47 +0800

    Update 0110-balanced-binary-tree.cpp

--- 0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
+        auto [balanced, h] = dfs(root);
+        return balanced;   
+    }
+
+    // return: {isBalanced, the height of root}
+    pair<bool, int> dfs(TreeNode* root) {
+        if (!root)
+            return {true, 0};
+        
+        auto [balancedLeft, hLeft] = dfs(root->left);
+        auto [balancedRight, hRight] = dfs(root->right); 
+
+        return {balancedLeft && balancedRight && abs(hLeft - hRight) <= 1, max(hLeft, hRight) + 1};
+    }
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
         if (!root)
             return true;
 
