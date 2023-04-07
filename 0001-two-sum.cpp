5142317cb95d65b2e7d5a96a35558dd359953df5
Michael <1906030+formichael@users.noreply.github.com>
2023-04-07 16:48:55 +0800

    Create 0001-two-sum.cpp

--- 0001-two-sum.cpp
+++ 0001-two-sum.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> vhelp;
-        vector<int> vres;
-        vector<int>::iterator it;
-        int i = 0;
-        for(it = nums.begin(); it != nums.end(); it++)
-        {
-            if(!vhelp.empty())
-            {
-                for(int j = 0; j < vhelp.size(); j++)
-                {
-                    if(*it + vhelp[j] == target)
-                    {
-                        vres.push_back(j);
-                        vres.push_back(i);
-                        return vres;
-                    }
-                }
-                vhelp.push_back(*it);
+        map<int, int> m; // num -> idx
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (auto itr = m.find(target - nums[i]); itr != m.end()) {
+                return {itr->second, i};
             }
-            else
-            {
-                vhelp.push_back(*it);
-            }
-            i++;
+            m[nums[i]] = i;
         }
-        return vres;
+
+        return {};
     }
 };
