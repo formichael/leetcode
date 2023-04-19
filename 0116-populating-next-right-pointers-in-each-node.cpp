c261ebf5702e4cde13c0cf2ef5c5106bbc8a290c
Michael <bymywill@outlook.com>
2023-04-19 22:36:43 +0800

    Create 0116-populating-next-right-pointers-in-each-node.cpp

---
+++ 0116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root)
+            return nullptr;
+
+        queue<Node*> q({root});
+        Node* pre = nullptr;
+
+        while(!q.empty()) {
+            int qsize = q.size();
+            for (int i = qsize; i > 0; i--) {
+                Node* node = q.front();
+                q.pop();
+
+                if (pre && i != qsize)
+                    pre->next = node;
+
+                pre = node;
+                
+                if (node->left)
+                    q.push(node->left);
+                
+                if (node->right)
+                    q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
