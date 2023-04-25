296bb127d99c2f60577d1025eca4b2b8fb8134fd
Michael <1906030+formichael@users.noreply.github.com>
2023-04-25 23:28:40 +0800

    Update 0002-add-two-numbers.cpp

--- 0002-add-two-numbers.cpp
+++ 0002-add-two-numbers.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* l = NULL;
-        ListNode* l3 = NULL;
-        int r = 0;
-        
-        for(;;) {
-            int t = r;
-            r = 0;
-            if (l1 == NULL && l2 == NULL && t == 0) {
-                break;
-            }
-            if (l1 != NULL) {
-                t += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != NULL) {
-                t += l2->val;
-                l2 = l2 -> next;
-            }
-            if (t > 9) {
-                r += 1;
-                t -= 10;
-            }
+        ListNode* head = nullptr;
+
+        ListNode *c1 = l1, *c2 = l2, *cur = nullptr;
+        int8_t carry = 0;
+
+        while (c1 && c2) {
+            int8_t sum = c1->val + c2->val + carry;
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
             
-            ListNode* l4 = new ListNode(t);
-            if (l3 == NULL) {
-                l3 = l4;
-                l = l3;
+            if (!head) {
+                head = node;
             } else {
-                l3->next = l4;
-                l3 = l3->next;
+                cur->next = node;
+                cur = node;
             }
+
+            cur = node;
+            c1 = c1->next;
+            c2 = c2->next;
         }
-        
-        return l;
+
+        while (c1) {
+            int8_t sum = c1->val + carry;
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
+            
+            if (!head) {
+                head = node;
+            } else {
+                cur->next = node;
+                cur = node;
+            }
+
+            cur = node;
+            c1 = c1->next;
+        }
+
+        while (c2) {
+            int8_t sum = c2->val + carry;
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
+            
+            if (!head) {
+                head = node;
+            } else {
+                cur->next = node;
+                cur = node;
+            }
+
+            cur = node;
+            c2 = c2->next;
+        }
+
+        if (carry)
+            cur->next = new ListNode(carry);
+
+        return head;
     }
 };
